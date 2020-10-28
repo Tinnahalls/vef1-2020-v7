7e69402b9c5b4e945d60b5bb86310010f05770e1
Tinnahalls <tinnahalls@gmail.com>
2020-10-28 22:33:59 +0000

    WOrking on project

--- scripts.js
+++ scripts.js
@@ -1,7 +1,59 @@
 
+function getPromptFromUser() {
+  let code = prompt("Hvort viltu kóða eða afkóða streng?");
 
-  var name = prompt("Please enter your name", "Micheal");
-  if (name != null) {
-    "Hello " + name + "! How are you today?";
+   if (code === "kóða"){
+    numberEncode();
+   }
+
+   else if(code === "afkóða") {
+    numberDecode();
+   }
+
+    else {
+      alert("Veit ekki hvaða aðgerð er. Reyndu aftur");
+      getPromptFromUser();
+    }
+  }
 
 
+
+  function numberDecode() {
+    let number = prompt("Hversu mikið á að hliðra streng? Gefðu upp heiltölu á bilinu 1 - 31");
+
+    if(number > 1 && number < 31) {
+      var string = prompt("Skrifaðu orð sem þú vilt hliðra")
+    Encode(); // náði bara að reyna að skrifa encode function
+
+    }
+    else {
+
+      alert("Veit ekki hvaða aðgerð er. Reyndu aftur.");
+      numberDecode();
+    }
+  }
+
+
+
+function numberEncode() {
+  let number = prompt("Hversu mikið á að hliðra streng? Gefðu upp heiltölu á bilinu 1 - 31");
+
+    if(number > 1 && number < 31) {
+      var string = prompt("Skrifaðu orð sem þú vilt hliðra")
+    Encode();
+
+    }
+    else{
+      alert("Veit ekki hvaða aðgerð er. Reyndu aftur.");
+      numberEncode();
+    }
+  }
+
+function Encode(string, number) {
+  var newstring = string
+  var n = str.charCodeAt(newstring.length-number);
+}
+
+function Decode() {
+  alert("Sorry náði ekki að klára");
+}
